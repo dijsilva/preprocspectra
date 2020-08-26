a6d50e4a536a39db8bb1f270471eeef69cc7de1b
Diego José da Silva <diegojsilvabr@gmail.com>
2020-08-26 14:06:57 -0300

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="preprocspectra",
-    version="0.0.1",
+    version="0.0.2",
     license='MIT',
     author="Diego José da Silva",
     author_email="diegojsilvabr@gmail.com",
